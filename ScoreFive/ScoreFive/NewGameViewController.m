90a0660c2e7adf0e88d91171c1a5989465540636
Varun Santhanam <talkto@vsanthanam.com>
2018-06-09 17:53:57 -0700

    added table view in new game view controller

--- ScoreFive/ScoreFive/NewGameViewController.m
+++ ScoreFive/ScoreFive/NewGameViewController.m
@@ -8,30 +8,30 @@
 
 #import "NewGameViewController.h"
 
-@interface NewGameViewController ()
+@interface NewGameViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation NewGameViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 0;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 0;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return nil;
+    
 }
-*/
 
 @end
